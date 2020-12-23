412b55cc50e50a0e97774f2e7243c2dee1f8858a
Shuvo <meghsohor@yahoo.com>
2020-12-23 10:50:10 +0600

    Validators added

---
+++ utils/validators.js
@@ -0,0 +1,22 @@
+module.exports.validateRegisterInput = (
+  username, email, password, confirmPassword
+) => {
+  const errors = {};
+
+  if (username.trim() === '') {
+    errors.username = 'Username must not be empty';
+  }
+  if(email.trim() === '') {
+    errors.email = "Email must not be empty";
+  } else {
+    const emailRegex = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(emailRegex)) {
+      errors.email = "Invalid email address";
+    }
+  }
+  if (password === '') {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords didn't";
+  }
+}
